d666e1b873e2084891152ae8cb0456ebbd6d68ae
levir24 <levi.richer06@gmail.com>
2024-04-29 10:56:19 -0400

    testing

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
 # Use an official Python runtime as a parent image
 
-FROM 
-python:3.9-slim
+FROM python:3.9-slim
 
 # Set the working directory to /app
-WORKDIR 
-/mqttpaho
+WORKDIR /mqttpaho
 
 # Copy the current directory contents into the container at /app
 COPY requirements.txt .
@@ -15,8 +13,7 @@ COPY mqttpaho.py .
 RUN pip install --trusted-host pypi.python.org -r requirements.txt
 
 # Define environment variable
-ENV 
-NAME mqttpaho
+ENV NAME mqttpaho
 
 # Run app.py when the container launches
 CMD ["python", "mqttpaho.py"]
--- mqttpaho.py
+++ mqttpaho.py
@@ -30,7 +30,8 @@ async def setbulb(ip,bri):
     #print(p.emeter_realtime)  # Print out current emeter status
 
     #await p.turn_off()  # Turn the device off
-    await p.set_hsv(0,100,int(bri/2.55))
+    #await p.set_hsv(0,100,int(bri/2.55))
+    await p.set_brightness(int(bri/2.55))
 
 async def onbulb(ip,on):
     p = SmartBulb(ip)
@@ -43,14 +44,15 @@ async def onbulb(ip,on):
     else: await p.turn_off()  # Turn the device off
     #await p.set_hsv(0,100,bri/2.55)
 
-async def setplug(ip):
+async def setplug(ip,on):
     p = SmartPlug(ip)
 
     await p.update()  # Request the update
     #print(p.alias)  # Print out the alias
     #print(p.emeter_realtime)  # Print out current emeter status
 
-    await p.turn_off()  # Turn the device off
+    if on: await p.turn_on()  # Turn the device on
+    else: await p.turn_off()  # Turn the device off
 
 
 asyncio.run(discover())
@@ -65,12 +67,19 @@ def on_connect(client, userdata, flags, reason_code, properties):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     global devip
-    s1=bytes(msg.payload).decode()
-    target=msg.topic[9:]
-    d1 = json.loads(s1)
-    print(msg.topic+" "+msg.topic[9:]+" "+s1,json.loads(s1))
-    if 'bri' in d1.keys(): asyncio.run(setbulb(devip[target],d1['bri']))
-    if 'on' in d1.keys(): asyncio.run(onbulb(devip[target],d1['on']))
+    global devtype
+    try:
+        s1=bytes(msg.payload).decode()
+        d1 = json.loads(s1)
+        target=msg.topic[9:]
+        print(msg.topic+" "+msg.topic[9:]+" "+s1,devtype[target])
+        if devtype[target] == 'Bulb':
+          if 'bri' in d1.keys(): asyncio.run(setbulb(devip[target],d1['bri']))
+          if 'on' in d1.keys(): asyncio.run(onbulb(devip[target],d1['on']))
+        else:
+          asyncio.run(setplug(devip[target],d1['on']))
+    except:
+        print("error occured")
 
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
